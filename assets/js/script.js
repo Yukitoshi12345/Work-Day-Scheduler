a7b510c672bd3fe32090d0ace5a0fca17e2ef9a1
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2023-12-19 12:57:33 +1100

    added comments for head tags

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// Moment script
+var now = moment();
+var formattedDate = now.format("dddd, MMMM DD, YYYY [at] h:mm A");
+console.log(formattedDate);
+// Day, date, time
+$("#currentDay").text(moment().format('LLLL'));
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
