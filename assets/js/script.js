889db6d1deab4443b8a692eecc2a84a15fce8e49
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2023-12-21 01:30:38 +1100

    added date and time, and also colour block in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,46 @@
 // Shows Day, Date, and Time
 // $("#currentDay") is jQuery. It is equivalent to saying document.getElementbyID("currentDay").
-$("#currentDay").text(moment().format('LLLL'));
+// $("#currentDay").text(moment().format('LLLL'));
 // format('L') gives MM/DD/YYYY. format('LL") gives [Month] [Day], [Year].
 // format('LLL') gives [Month] [Day], [Year] [Time]. format('LLLL') gives [Monday/.../Sunday], [Month] [Day], [Year] [Time].
 //.text() change its content.
 
+var currentHourSlot = dayjs().format('H');
+var currentDayEl = $('#currentDay');
+
+function displayTime() {
+    var currentLocalTime = dayjs().format('MMM DD, YYYY hh:mm:ss a');
+    currentDayEl.text(currentLocalTime);
+  }
 
 
+function colourHourlyChange() {
+    $('.time-block').each(function() {
+        // parseInt function converts a string value to an integer.
+        var timeHourSlot = parseInt($(this).attr("id"));
+        console.log(timeHourSlot);
 
+        if (currentHourSlot < timeHourSlot) {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
 
-// $(function () {
+        else if (currentHourSlot > timeHourSlot) {
+            $(this).addClass("past");
+            $(this).removeClass("present");
+            $(this).removeClass("future");
+        }
 
-// });
+        else {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        }
 
+    });
+    
+}
 
+displayTime();
+setInterval(displayTime, 1000);
