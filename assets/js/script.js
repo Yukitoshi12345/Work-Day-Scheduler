d3addfd184a0046e70d47053fac4bb581bf73056
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2023-12-21 00:26:48 +1100

    attached day js in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
-// Day, date, time
+// Shows Day, Date, and Time
+// $("#currentDay") is jQuery. It is equivalent to saying document.getElementbyID("currentDay").
 $("#currentDay").text(moment().format('LLLL'));
 // format('L') gives MM/DD/YYYY. format('LL") gives [Month] [Day], [Year].
 // format('LLL') gives [Month] [Day], [Year] [Time]. format('LLLL') gives [Monday/.../Sunday], [Month] [Day], [Year] [Time].
 //.text() change its content.
+
 
 
 
