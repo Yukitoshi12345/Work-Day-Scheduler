77ebf422bfb626e9a870ac6bfbfa50cdb85dc4c0
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2023-12-21 01:40:03 +1100

    added comments in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,12 +8,13 @@
 var currentHourSlot = dayjs().format('H');
 var currentDayEl = $('#currentDay');
 
+// This function is to get the real time
 function displayTime() {
-    var currentLocalTime = dayjs().format('MMM DD, YYYY hh:mm:ss a');
+    var currentLocalTime = dayjs().format('dddd, MMM DD, YYYY hh:mm:ss a');
     currentDayEl.text(currentLocalTime);
   }
 
-
+// This function is to show the colour blocks dependent on if it is past/present/future hour block
 function colourHourlyChange() {
     $('.time-block').each(function() {
         // parseInt function converts a string value to an integer.
@@ -21,26 +22,28 @@ function colourHourlyChange() {
         console.log(timeHourSlot);
 
         if (currentHourSlot < timeHourSlot) {
-            $(this).removeClass("past");
-            $(this).removeClass("present");
+            // $(this).removeClass("past");
+            // $(this).removeClass("present");
             $(this).addClass("future");
         }
 
         else if (currentHourSlot > timeHourSlot) {
             $(this).addClass("past");
-            $(this).removeClass("present");
-            $(this).removeClass("future");
+            // $(this).removeClass("present");
+            // $(this).removeClass("future");
         }
 
+        // else currentHourSlot === timeHourSlot:
         else {
-            $(this).removeClass("past");
+            // $(this).removeClass("past");
             $(this).addClass("present");
-            $(this).removeClass("future");
+            // $(this).removeClass("future");
         }
 
     });
-    
+
 }
 
 displayTime();
+colourHourlyChange();
 setInterval(displayTime, 1000);
